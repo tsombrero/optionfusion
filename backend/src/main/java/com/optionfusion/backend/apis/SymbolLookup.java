c9134a85877bb0403579bf6b5457a7cb07159ae4
Jim Kerr <jimkerr@cisco.com>
2016-01-19 07:20:21 -0800

    Add Backend template

---
+++ backend/src/main/java/com/optionfusion/backend/apis/SymbolLookup.java
@@ -0,0 +1,36 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.optionfusion.backend.apis;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.optionfusion.backend.models.Symbol;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.inject.Named;
+
+/** An endpoint class we are exposing */
+@Api(
+  name = "optionFusion",
+  version = "v1",
+  namespace = @ApiNamespace(
+    ownerDomain = "backend.optionfusion.com",
+    ownerName = "backend.optionfusion.com",
+    packagePath=""
+  )
+)
+public class SymbolLookup {
+
+    @ApiMethod(httpMethod="GET")
+    public final List<Symbol> getMatching(@Named("q") String searchString) {
+        return Collections.singletonList(new Symbol("FOO", "Foobar & Co"));
+    }
+
+}
---
+++ backend/src/main/java/com/optionfusion/backend/models/Symbol.java
@@ -0,0 +1,35 @@
+package com.optionfusion.backend.models;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
+public class Symbol {
+
+    @Id
+    private String symbol;
+
+    private String description;
+
+    public Symbol(String symbol, String description) {
+
+        this.symbol = symbol;
+        this.description = description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
